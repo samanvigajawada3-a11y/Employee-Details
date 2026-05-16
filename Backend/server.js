52c51695ee1a9f0307e3a5bb994a510163d25f77
samanvigajawada3-a11y <samanvigajawada3@gmail.com>
2026-05-16 15:29:14 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,41 @@
 import exp from "express"
-import {employeeApp} from "./API/EmployeeAPI.js"
-import {connect} from "mongoose"
+import { employeeApp } from "./API/EmployeeAPI.js"
+import { connect } from "mongoose"
 import cors from "cors"
-
+import dotenv from "dotenv"
+// configure dotenv
+dotenv.config()
 const app = exp()
-
-// add cors middleware
-//CORS (Cross-Origin Resource Sharing) is used to allow your frontend and backend to talk to each other when they are on different origins.
+// CORS middleware
 app.use(cors({
-    origin : ["http://localhost:5173"]
+    origin: ["http://localhost:5173"]
 }))
-
-// body parser midleware
+// body parser middleware
 app.use(exp.json())
 // employee api middleware
-app.use("/employee-api",employeeApp)
-
+app.use("/employee-api", employeeApp)
 // port number
-const port = 3000
-
+const port = process.env.PORT || 3000
 // connect to DB
-async function connectDB(){
-    try{
-        await connect(DB_URL)
+async function connectDB() {
+    try {
+        // connect to MongoDB Atlas
+        await connect(process.env.DB_URL)
         console.log("DB connection success")
-        // conection
-        app.listen(port,()=>console.log(`Server is listening to the port ${port}.....`))
-    }catch(err){
-        console.log("Error occured in DB : ",err.message)
+        // start server
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}.....`)
+        })
+    } catch (err) {
+        console.log("Error occurred in DB:", err.message)
     }
 }
 connectDB()
-
 // Error handling middleware
-app.use((err,req,res,next)=>{
-    res.status(err.status || 500).json({
-        message : "error",
-        reason : err.message
-    })
+app.use((err, req, res, next) => {
+ res.status(err.status || 500).json({
+        message: "error",
+        reason: err.message
+ })
 })
+
